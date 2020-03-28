d766bf95f10b9a29f1e3269bfb494766b0713e61
Sai Deekshith <msaideekshith@gmail.com>
2020-03-28 18:35:21 +0530

    generic stack

---
+++ GenericStack/GenericStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericStack
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> stackList = new Stack<string>();
+
+            stackList.Push("AAA");
+            stackList.Push("BBB");
+            stackList.Push("CCC");
+            stackList.Push("DDD");
+            stackList.Push("EEE");
+
+            Console.WriteLine("What is on the top is " + stackList.Peek());
+            stackList.Pop();
+            stackList.Pop();
+            Console.WriteLine("What is on the top is " + stackList.Peek());
+            foreach (var item in stackList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
